0efff282c7523f00e6dba398e697763e75a539ea
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-09-13 08:34:54 -0400

    Improving the commenting in the GasMileage.java.

--- chapter01to03/GasMileage.java
+++ chapter01to03/GasMileage.java
@@ -1,16 +1,22 @@
-//********************************************************************
-//  GasMileage.java       Author: Lewis/Loftus
-//
-//  Demonstrates the use of the Scanner class to read numeric data.
-//********************************************************************
-
 import java.util.Scanner;
 
+/**
+ * This class has a method that accepts user input and displays it as output.
+ * The program will make a prompt, accept input and then make another prompt
+ * and accept input another time. This program can calculate gas mileage.
+ *
+ * <p>Bugs: This program has no known bugs.
+ *
+ * @author Lewis and Loftus
+ */
+
 public class GasMileage {
-  //-----------------------------------------------------------------
-  //  Calculates fuel efficiency based on values entered by the
-  //  user.
-  //-----------------------------------------------------------------
+
+  /**
+   * Demonstrate the use of the Scanner class to read user input.
+   * Compile this program: javac GasMileage.java
+   * Run this program: java GasMileage
+   */
   public static void main(String[] args) {
     int miles;
     double gallons;
