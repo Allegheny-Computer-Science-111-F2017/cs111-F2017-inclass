89875f714fd55b60252bbc7a0faea4db87b1cda6
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-09-13 08:28:59 -0400

    Improving the formatting in the GasMileage.java. :scream_cat:

--- chapter01to03/GasMileage.java
+++ chapter01to03/GasMileage.java
@@ -6,27 +6,26 @@
 
 import java.util.Scanner;
 
-public class GasMileage
-{
-    //-----------------------------------------------------------------
-    //  Calculates fuel efficiency based on values entered by the
-    //  user.
-    //-----------------------------------------------------------------
-    public static void main (String[] args)
-    {
-        int miles;
-        double gallons, mpg;
+public class GasMileage {
+  //-----------------------------------------------------------------
+  //  Calculates fuel efficiency based on values entered by the
+  //  user.
+  //-----------------------------------------------------------------
+  public static void main(String[] args) {
+    int miles;
+    double gallons;
+    double mpg;
 
-        Scanner scan = new Scanner (System.in);
+    Scanner scan = new Scanner(System.in);
 
-        System.out.print ("Enter the number of miles: ");
-        miles = scan.nextInt();
+    System.out.print("Enter the number of miles: ");
+    miles = scan.nextInt();
 
-        System.out.print ("Enter the gallons of fuel used: ");
-        gallons = scan.nextDouble();
+    System.out.print("Enter the gallons of fuel used: ");
+    gallons = scan.nextDouble();
 
-        mpg = miles / gallons;
+    mpg = miles / gallons;
 
-        System.out.println ("Miles Per Gallon: " + mpg);
-    }
+    System.out.println("Miles Per Gallon: " + mpg);
+  }
 }
