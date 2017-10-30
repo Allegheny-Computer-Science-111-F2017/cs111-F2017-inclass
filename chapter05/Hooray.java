c2672e4dfe2fc9273fec6a54bd9cbb80bccb1e93
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-10-30 12:11:01 -0400

    Improving the formatting and the comments in the Hooray file.

--- chapter05/Hooray.java
+++ chapter05/Hooray.java
@@ -1,23 +1,27 @@
-public class Hooray
-{
+public class Hooray {
 
-  public static void hooray()
-  {
-
-    while(true)
-    {
-
+  /**
+   * Demonstrates the use of an infinite while loop.
+   *
+   * <p>Bugs: This program has a known bug.
+   *
+   * @author Kapfhammer
+   */
+  public static void hooray() {
+    while (true) {
       System.out.println("Hooray!");
-
     }
-
   }
 
-  public static void main(String[] args)
-  {
-
+  /**
+   * Demonstrates the calling of a method with a loop.
+   * Compile this program: javac Hooray.java
+   * Run this program: java Hooray
+   * Remember, this program runs infinitely.
+   * To stop it from running, press CTRL-c in terminal.
+   */
+  public static void main(String[] args) {
     hooray();
-
   }
 
 }
