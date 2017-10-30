e05105dfc4e271005b70f8643e39923f678a9d1b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-10-30 10:46:21 -0400

    Improving the formatting and the naming in the LogisticMap.

--- chapter01to03/LogisticMap.java
+++ chapter01to03/LogisticMap.java
@@ -1,15 +1,14 @@
-import java.util.*;
-import java.text.*;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 /**
  *  Compilation:  javac LogisticMap.java
  *  Execution:    java LogisticMap 0 or
  *                java LogisticMap (number from previous output)
  *
- *  Plot the bifurcation diagram of the logistic map. The logistic
+ *  <p>Plot the bifurcation diagram of the logistic map. The logistic
  *  is: x[n+1] = 4 r x[n](1 - x[n]). For each value of r, discard
  *  the first 1000 iterates x[n], then plot the next 100.
- *
  */
 
 public class LogisticMap {
@@ -21,18 +20,15 @@ public class LogisticMap {
     return 4.0 * r * y * (1.0 - y);
   }
 
- /**
-   * This is the main method that iteratively computes and displays.
-   * Notice that this method displays graphics at the pixel-by-pixel level
-   * instead of using other methods that can draw and entire shape.
-   */
+  /**
+    * This is the main method that iteratively computes and displays.
+    * numberOfIterationsotice that this method displays graphics at the pixel-by-pixel level
+    * instead of using other methods that can draw and entire shape.
+    */
   public static void main(String[] args) {
 
     // store the values
-    ArrayList values = new ArrayList();
-
-    // Number of iterations
-    int N = 800;
+    ArrayList<Double> values = new ArrayList<Double>();
 
     // grab a value so that we can output certain scores
     Double rchosenFirst = new Double(args[0]);
@@ -43,25 +39,27 @@ public class LogisticMap {
     StdDraw.setYscale(0.0, 1.0);
 
     // shows how to put the "origin" into the graph
-    StdDraw.text(.7,0,".7");
+    StdDraw.text(.7, 0, ".7");
     StdDraw.show(10);
 
-    // draw the entire bifurcation map
-    for (double r = 0.7; r <= 1.0; r += 0.3/N) {
+    // numberOfIterationsumber of iterations
+    int numberOfIterations = 800;
 
+    // draw the entire bifurcation map
+    for (double r = 0.7; r <= 1.0; r += 0.3 / numberOfIterations) {
       // choose random initial value
       double y = Math.random();
 
       // ignore first 1000 iterates
-      for (int i = 0; i < 1000; i++)
+      for (int i = 0; i < 1000; i++) {
         y = logistic(y, r);
+      }
 
       // reinitialize the array that will store the values
-      values = new ArrayList();
+      values = new ArrayList<Double>();
 
       // plot next 100 iterates
       for (int i = 0; i < 100; i++) {
-
         // call the function again and then plot these
         // points on the graphical interface using the
         // StdDraw package -- using (r,y) points
@@ -70,7 +68,7 @@ public class LogisticMap {
 
         // add all of the values for
         // the command-line argument
-        if( rchosen == roundSixDecimals(r) ) {
+        if (rchosen == roundSixDecimals(r)) {
           values.add(y);
         }
       }
@@ -80,7 +78,7 @@ public class LogisticMap {
 
       // print some of the the debugging output so that we
       // can check for a pattern in the output
-      if( rchosen == roundSixDecimals(r) ) {
+      if ( rchosen == roundSixDecimals(r) ) {
         System.out.println("r = " + rchosen);
         System.out.println(values.toString());
       }
@@ -88,7 +86,7 @@ public class LogisticMap {
       // print all of the values for debugging purposes
       // note that this will produce a substantial amount
       // of output in the terminal window of your program!
-      if( rchosen == 0 ) {
+      if ( rchosen == 0 ) {
         System.out.println("r = " + r);
       }
     }
@@ -97,10 +95,9 @@ public class LogisticMap {
   /**
    * Round the number of decimals so that output is easier to read.
    */
-  public static double roundSixDecimals(double d) {
+  public static double roundSixDecimals(double value) {
     DecimalFormat sixDForm = new DecimalFormat("#.######");
-    return Double.valueOf(sixDForm.format(d));
+    return Double.valueOf(sixDForm.format(value));
   }
 
 }
-
