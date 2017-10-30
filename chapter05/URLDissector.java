e01ede53b16347a52a94e846107fd344deab6c91
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-10-30 16:33:32 -0400

    Improving the URLDissector; name is still not conforming.

--- chapter05/URLDissector.java
+++ chapter05/URLDissector.java
@@ -5,34 +5,42 @@
 //  using alternative delimiters.
 //********************************************************************
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
-import java.io.*;
 
-public class URLDissector
-{
-  //-----------------------------------------------------------------
-  //  Reads urls from a file and prints their path components.
-  //-----------------------------------------------------------------
-  public static void main (String[] args) throws IOException
-  {
+/** Demonstrates the use of the java.util.Scanner class to read file input and
+ * to parse it using some alternative delimiters.
+ *
+ * <p>Bugs: This program has no known bugs.
+ *
+ * @author Lewis and Loftus
+ */
+public class URLDissector {
+
+  /**
+   * Demonstrates the use of the Scanner class to read input from a file.
+   * Compile this program: javac URLDissector.java
+   * Run this program: java URLDissector
+   */
+  public static void main(String[] args) throws IOException {
     String url;
-    Scanner fileScan, urlScan;
+    Scanner fileScan;
+    Scanner urlScan;
+    fileScan = new Scanner(new File("urls.inp"));
 
-    fileScan = new Scanner (new File("urls.inp"));
-
-    // Read and process each line of the file
-    while (fileScan.hasNext())
-    {
+    // read and process each line of the file
+    while (fileScan.hasNext()) {
       url = fileScan.nextLine();
-      System.out.println ("URL: " + url);
+      System.out.println("URL: " + url);
 
-      urlScan = new Scanner (url);
+      urlScan = new Scanner(url);
       urlScan.useDelimiter("/");
 
-      //  Print each part of the url
-      while (urlScan.hasNext())
-        System.out.println ("   " + urlScan.next());
-
+      // print each part of the url
+      while (urlScan.hasNext()) {
+        System.out.println("   " + urlScan.next());
+      }
       System.out.println();
     }
   }
