70ed7f316e5a97b0b662de1b4cb82d35b5086734
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-10-30 11:49:00 -0400

    Reorganizing all of the code in the Transactions file.

--- chapter04/Transactions.java
+++ chapter04/Transactions.java
@@ -1,5 +1,6 @@
 /**
  * Demonstrates the creation and use of multiple Accounts.
+ *
  * <p>Bugs: This program has no known bugs.
  *
  * @author Lewis and Loftus
@@ -10,23 +11,25 @@ public class Transactions {
    *  Compile this program: javac Transactions.java
    *  Run this program: java Transactions
    */
-  public static void main (String[] args) {
+  public static void main(String[] args) {
     Account acct1 = new Account("Ted Murphy", 72354, 102.56);
     Account acct2 = new Account("Jane Smith", 69713, 40.00);
-    Account acct3 = new Account("Edward Demsey", 93757, 759.32);
 
+    // perform deposits and withdraws on an account
     acct1.deposit(25.85);
-
     double smithBalance = acct2.deposit(500.00);
     System.out.println("Smith balance after deposit: " + smithBalance);
-
     System.out.println("Murphy balance after withdrawal: "
                         + acct2.withdraw(430.75, 1.50));
 
+    Account acct3 = new Account("Edward Demsey", 93757, 759.32);
+
+    // add interest to the accounts
     acct1.addInterest();
     acct2.addInterest();
     acct3.addInterest();
 
+    // display the status of the accounts
     System.out.println();
     System.out.println(acct1);
     System.out.println(acct2);
