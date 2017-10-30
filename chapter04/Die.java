a9dc982965ba358da476a82b39e76347d884152f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-10-30 11:52:26 -0400

    Resolving many formatting issues in the Die file.

--- chapter04/Die.java
+++ chapter04/Die.java
@@ -1,57 +1,39 @@
-//********************************************************************
-//  Die.java       Author: Lewis/Loftus
-//
-//  Represents one die (singular of dice) with faces showing values
-//  between 1 and 6.
-//********************************************************************
+/**
+ * This class represents one die (singular of dice) with faces showing
+ * values between one and six.
+ *
+ * <p>Bugs: This program has no known bugs.
+ *
+ * @author Lewis and Loftus
+ */
+public class Die {
+  private static final int MAX = 6;
+  private int faceValue;
 
-public class Die
-{
-   private final int MAX = 6;  // maximum face value
+  /** Constructor: Sets the initial face value. */
+  public Die() {
+    faceValue = 1;
+  }
 
-   private int faceValue;  // current value showing on the die
+  /** Rolls the die and returns the result. */
+  public int roll() {
+    faceValue = (int)(Math.random() * MAX) + 1;
+    return faceValue;
+  }
 
-   //-----------------------------------------------------------------
-   //  Constructor: Sets the initial face value.
-   //-----------------------------------------------------------------
-   public Die()
-   {
-      faceValue = 1;
-   }
+  /** Face value mutator. */
+  public void setFaceValue(int value) {
+    faceValue = value;
+  }
 
-   //-----------------------------------------------------------------
-   //  Rolls the die and returns the result.
-   //-----------------------------------------------------------------
-   public int roll()
-   {
-      faceValue = (int)(Math.random() * MAX) + 1;
+  /** Face value accessor. */
+  public int getFaceValue() {
+    return faceValue;
+  }
 
-      return faceValue;
-   }
-
-   //-----------------------------------------------------------------
-   //  Face value mutator.
-   //-----------------------------------------------------------------
-   public void setFaceValue (int value)
-   {
-      faceValue = value;
-   }
-
-   //-----------------------------------------------------------------
-   //  Face value accessor.
-   //-----------------------------------------------------------------
-   public int getFaceValue()
-   {
-      return faceValue;
-   }
-
-   //-----------------------------------------------------------------
-   //  Returns a string representation of this die.
-   //-----------------------------------------------------------------
-   public String toString()
-   {
-      String result = Integer.toString(faceValue);
-
-      return result;
-   }
+  /** Returns a string representation of this die. */
+  public String toString() {
+    String result = Integer.toString(faceValue);
+    return result;
+  }
 }
