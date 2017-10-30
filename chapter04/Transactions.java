5aa6083a5c42d5365d5b55e336e6e0d84b5713cb
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-10-30 11:45:58 -0400

    Updating all of the formatting and comments in the Transactions.

--- chapter04/Transactions.java
+++ chapter04/Transactions.java
@@ -1,36 +1,35 @@
-//********************************************************************
-//  Transactions.java       Author: Lewis/Loftus
-//
-//  Demonstrates the creation and use of multiple Account objects.
-//********************************************************************
+/**
+ * Demonstrates the creation and use of multiple Accounts.
+ * <p>Bugs: This program has no known bugs.
+ *
+ * @author Lewis and Loftus
+ */
+public class Transactions {
 
-public class Transactions
-{
-   //-----------------------------------------------------------------
-   //  Creates some bank accounts and requests various services.
-   //-----------------------------------------------------------------
-   public static void main (String[] args)
-   {
-      Account acct1 = new Account ("Ted Murphy", 72354, 102.56);
-      Account acct2 = new Account ("Jane Smith", 69713, 40.00);
-      Account acct3 = new Account ("Edward Demsey", 93757, 759.32);
+  /** Creates some bank accounts and requests various services.
+   *  Compile this program: javac Transactions.java
+   *  Run this program: java Transactions
+   */
+  public static void main (String[] args) {
+    Account acct1 = new Account("Ted Murphy", 72354, 102.56);
+    Account acct2 = new Account("Jane Smith", 69713, 40.00);
+    Account acct3 = new Account("Edward Demsey", 93757, 759.32);
 
-      acct1.deposit (25.85);
+    acct1.deposit(25.85);
 
-      double smithBalance = acct2.deposit (500.00);
-      System.out.println ("Smith balance after deposit: " +
-                          smithBalance);
+    double smithBalance = acct2.deposit(500.00);
+    System.out.println("Smith balance after deposit: " + smithBalance);
 
-      System.out.println ("Murphy balance after withdrawal: " + 
-                          acct2.withdraw (430.75, 1.50));
+    System.out.println("Murphy balance after withdrawal: "
+                        + acct2.withdraw(430.75, 1.50));
 
-      acct1.addInterest();
-      acct2.addInterest();
-      acct3.addInterest();
+    acct1.addInterest();
+    acct2.addInterest();
+    acct3.addInterest();
 
-      System.out.println ();
-      System.out.println (acct1);
-      System.out.println (acct2);
-      System.out.println (acct3);
-   }
+    System.out.println();
+    System.out.println(acct1);
+    System.out.println(acct2);
+    System.out.println(acct3);
+  }
 }
