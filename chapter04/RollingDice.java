56f92c4704f8cf578239c11faaf3e5529c480494
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-10-30 12:01:22 -0400

    Finishing all of the reformatting of the code for the RollingDice.

--- chapter04/RollingDice.java
+++ chapter04/RollingDice.java
@@ -1,35 +1,38 @@
-//********************************************************************
-//  RollingDice.java       Author: Lewis/Loftus
-//
-//  Demonstrates the creation and use of a user-defined class.
-//********************************************************************
+/**
+ * Demonstrates the creation and use of a user-defined class.
+ *
+ * <p>Bugs: This program has no known bugs.
+ *
+ * @author Lewis and Loftus
+ */
+public class RollingDice {
 
-public class RollingDice
-{
-   //-----------------------------------------------------------------
-   //  Creates two Die objects and rolls them several times.
-   //-----------------------------------------------------------------
-   public static void main (String[] args)
-   {
-      Die die1, die2;
-      int sum;
+  /**
+   * Demonstrate the use of creating and using objects.
+   * Compile this program: javac RollingDice.java
+   * Run this program: java RollingDice
+   */
+  public static void main(String[] args) {
+    Die die1;
+    Die die2;
 
-      die1 = new Die();
-      die2 = new Die();
+    die1 = new Die();
+    die2 = new Die();
 
-      die1.roll();
-      die2.roll();
-      System.out.println ("Die One: " + die1 + ", Die Two: " + die2);
+    die1.roll();
+    die2.roll();
+    System.out.println("Die One: " + die1 + ", Die Two: " + die2);
 
-      die1.roll();
-      die2.setFaceValue(4);
-      System.out.println ("Die One: " + die1 + ", Die Two: " + die2);
+    die1.roll();
+    die2.setFaceValue(4);
+    System.out.println("Die One: " + die1 + ", Die Two: " + die2);
 
-      sum = die1.getFaceValue() + die2.getFaceValue();
-      System.out.println ("Sum: " + sum);
+    int sum;
+    sum = die1.getFaceValue() + die2.getFaceValue();
+    System.out.println("Sum: " + sum);
 
-      sum = die1.roll() + die2.roll();
-      System.out.println ("Die One: " + die1 + ", Die Two: " + die2);
-      System.out.println ("New sum: " + sum);
-   }
+    sum = die1.roll() + die2.roll();
+    System.out.println("Die One: " + die1 + ", Die Two: " + die2);
+    System.out.println("New sum: " + sum);
+  }
 }
